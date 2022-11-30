1f276854de947d2f88e46fc28e090609b0881ee7
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-11-30 06:42:56 +0100

    :hammer: added function that raise a number to some power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def pow(a: int, b: int) -> int:
+    """A function that computes a to the power
+    of b
+
+    Args:
+        a (int): Number to be raised to a power
+        b (int): Power to raise number to
+
+    Returns:
+        int: result from the raise to power operation
+    """
+    return a ^ b
