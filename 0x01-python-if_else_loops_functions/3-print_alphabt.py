de13e48f560526727aa76f9ca37854571c4d821d
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-11-30 05:14:47 +0100

    :hammer: another script for printing ascii

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""A Program that prints the ASCII alphabet, in lowercase
+    except 'q' and 'e'
+"""
+
+for character in range(97, 123):
+    if character != 101 and 113:
+        print("{}".format(chr(character)), end="")
