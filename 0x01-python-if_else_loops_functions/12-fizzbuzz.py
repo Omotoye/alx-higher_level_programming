152dac6c603924649aa51607356c79d76606c578
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-11-30 06:55:05 +0100

    :hammer: added script for fizzbuzz game

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz() -> None:
+    for num in range(1, 101):
+        if num % 3 == 0:
+            print("Fizz", end=" ")
+        elif num % 5 == 0:
+            print("Buzz", end=" ")
+        elif num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print(num, end=" ")
+    print()
