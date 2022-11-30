2f0374fedf66b0e5799aaa2552e534c192de4fd4
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-11-30 06:59:59 +0100

    :ambulance: fixed a logical error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,12 +3,12 @@
 
 def fizzbuzz() -> None:
     for num in range(1, 101):
-        if num % 3 == 0:
-            print("Fizz", end=" ")
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz", end=" ")
         elif num % 5 == 0:
             print("Buzz", end=" ")
-        elif num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz", end=" ")
+        elif num % 3 == 0:
+            print("Fizz", end=" ")
         else:
             print(num, end=" ")
     print()
