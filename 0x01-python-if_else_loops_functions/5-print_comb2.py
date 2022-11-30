dac125825e3b75f391f788b4309f4cc1b14c196f
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-11-30 05:43:44 +0100

    :construction: <testing>

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 
 for i in range(100):
     print(
-      "{}{}{}".format(0 if i < 10 else "", i, "," if i != 99 else ""), end=" "
+      "{}{}{}".format(0 if i < 10 else "", i, ", " if i != 99 else ""), end=""
     )
