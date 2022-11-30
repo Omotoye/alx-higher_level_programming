be125c9abbab8e5bb0380383b3716dcff10f454c
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-11-30 06:37:49 +0100

    :hammer: added function to add two numbers

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def add(a: int, b: int) -> int:
+    """Adds two integers
+
+    Args:
+        a (int): first integer
+        b (int): second integer
+
+    Returns:
+        int: result of the addition
+    """
+    return a + b
