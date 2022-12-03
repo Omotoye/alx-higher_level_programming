ed5bd2950224ef2d641453dbf4547157df9cc955
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-03 21:59:58 +0100

    :construction: testing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 
-def print_list_integer(my_list=[]) -> None:
+def print_list_integer(my_list: list[int] = []) -> None:
     """A function to print all the items contained in a given list`
 
     Args:
         my_list (list[int], optional): list to be printed. Defaults to [].
     """
-    for item in my_list:
-        print("{}".format(item))
+    if my_list:
+        for item in my_list:
+            print("{}".format(item))
