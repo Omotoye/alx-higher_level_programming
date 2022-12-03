bafe43f1018c90c2d49f6f65318ed5efc4954954
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-03 22:42:13 +0100

    :hammer: added function that reverses a list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    """A function that reverses a list
+
+    Args:
+        my_list (list[int], optional): list of integers. Defaults to [].
+    """
+    if my_list:
+        for index in range(-1, -len(my_list) - 1, -1):
+            print("{:d}".format(my_list[index]))
