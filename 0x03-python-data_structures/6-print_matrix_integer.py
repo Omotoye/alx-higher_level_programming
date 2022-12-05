ef0a23b23d8d945a77df6aed40142aa9f2cad44b
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-06 00:20:15 +0100

    :construction: testing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,9 +8,9 @@ def print_matrix_integer(matrix=[[]]):
         matrix (list[list[int]], optional): The matrix of integer to be
             printed. Defaults to [[]].
     """
-    for row in matrix:
-        if row:
-            for n in row:
-                print("{:d}{}".format(n, "\n" if n is row[-1] else " "), end="")
+    for rw in matrix:
+        if rw:
+            for n in rw:
+                print("{:d}{}".format(n, "\n" if n is rw[-1] else " "), end="")
         else:
             print()
