58fa865dc115812ce140c04895b762b7ed0cf884
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-07 19:11:32 +0100

    :recycle: removed type aliasing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
-from typing import List
 
-
-# Type Alias for matrix type
-Matrix = List[List[int]]
-
-
-def square_matrix_simple(matrix: Matrix = []) -> Matrix:
+def square_matrix_simple(matrix=[]):
     """A function that computes the square value of all integers
     of a matrix.
 
@@ -23,7 +17,7 @@ def square_matrix_simple(matrix: Matrix = []) -> Matrix:
         Matrix: The new matrix that contains the square values
             from the given matrix
     """
-    square_matrix: Matrix = []
+    square_matrix = []
     for row in matrix:
         square_matrix.append(list(map(lambda x: x * x, row)))
     return square_matrix
