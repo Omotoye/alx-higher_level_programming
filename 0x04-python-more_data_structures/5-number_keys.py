60b36887433ad7c0cac00307a13537b590ce752a
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-07 22:34:39 +0100

    added function to return the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary) -> int:
+    """A function that returns the number of keys in a dictionary
+
+    Type Alias:
+        Dictionary = dict[str, Any]
+
+    Args:
+        a_dictionary (Dictionary): dictionary to get number of keys
+            from
+
+    Returns:
+        int: the count of keys in the given dictionary
+    """
+    return len(a_dictionary)
