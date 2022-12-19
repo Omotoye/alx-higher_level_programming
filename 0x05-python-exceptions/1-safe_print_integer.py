c3d7dc86f4a0ad09e9eecd650b4da5a5e6f81f66
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-19 17:00:49 +0100

    :hammer: added a function that prints integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value) -> bool:
+    """A function that prints an integer with format method
+    if the value given is an integer
+
+    Args:
+        value (Any): value to be printed and it could be of
+            any type
+
+    Returns:
+        bool: `true` if `value` has been correctly printed (which
+            means the given `value` is an integer)
+    """
+    correct_printing: bool = False
+    try:
+        print("{:d}".format(value))
+        correct_printing = True
+    except ValueError:
+        pass
+    return correct_printing
