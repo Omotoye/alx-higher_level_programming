75d3ad14ac4f92207bfa1d0f11802a3a799107ca
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-19 18:19:16 +0100

    :hammer: added a funciton that raise type exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def raise_exception() -> None:
+    """A function that raises a type exception.
+
+    Raises:
+        TypeError: the exception to be raised
+    """
+    raise TypeError
