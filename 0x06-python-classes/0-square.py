3a2340e8940176a1dd964a154a61a216a656b569
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-12-20 19:38:53 +0100

    :construction: testing

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""This Module defines an empty class `Square` that defines a square
+
+This module demonstrates how to create a class by creating an empty class
+that would be added to in later modules 
+
+"""
 
 
 class Square:
-    pass
+    """An Empty class `Square` that defines a square"""
+    def __init__(self) -> None:
+        pass
